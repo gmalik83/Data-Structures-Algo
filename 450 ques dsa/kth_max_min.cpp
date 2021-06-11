346a7eece09ba1614fb495679dd827f9711f56d9
gmalik83 <g8383malik@gmail.com>
2021-06-11 22:09:05 +0530

    450 ques DSA added

---
+++ 450 ques dsa/kth_max_min.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    /*
+        Approaches :
+        1. Sort and return k-1 element from left for min and right for max
+        2. Use Min Heap Or Max Heap
+        3.
+    */
+    
+
+    return 0;
+}
