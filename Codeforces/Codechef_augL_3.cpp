02531970e4fc140fb0277ab6e923911dfa2573d7
gmalik83 <gaurav98malik@gmail.com>
2021-08-07 18:32:44 +0530

    Long 2

---
+++ Codeforces/Codechef_augL_3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, p, k;
+        cin >> n >> p >> k;
+        int ck = 0;
+        int i = 0;
+    }
+    return 0;
+}
