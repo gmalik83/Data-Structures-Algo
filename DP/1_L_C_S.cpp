3673e15cce606b87e930910ec45f4636f5b9d5e6
gmalik83 <gaurav98malik@gmail.com>
2021-09-12 15:34:52 +0530

    LCS top down

--- DP/1_L_C_S.cpp
+++ DP/1_L_C_S.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+int dp[101][101];
 int LCS(string str1, string str2, int m, int n)
 {
     if (m == 0 || n == 0)
         return 0;
+    if (dp[m][n] != -1)
+        return dp[m][n];
     if (str1[m - 1] == str2[n - 1])
-        return 1 + LCS(str1, str2, m - 1, n - 1);
+        return dp[m][n] = 1 + LCS(str1, str2, m - 1, n - 1);
     else
-        return max(LCS(str1, str2, m, n - 1), LCS(str1, str2, m - 1, n));
+        return dp[m][n] = max(LCS(str1, str2, m, n - 1), LCS(str1, str2, m - 1, n));
 }
 int main()
 {
     string str1 = "ABCDEFG";
     string str2 = "ACDE";
+    memset(dp, -1, sizeof(dp));
     cout << LCS(str1, str2, 7, 4);
 }
