24712c2db75878607bf2069cf0a7d2b7da4ac760
gmalik83 <gaurav98malik@gmail.com>
2021-10-12 22:32:13 +0530

    MCM

--- DP/MCM.cpp
+++ DP/MCM.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+int dp[101][101];
 
 int MCM(int pi[], int i, int j)
 {
     if (i == j)
         return 0;
+    if (dp[i][j] != -1)
+        return dp[i][j];
     int k;
     int min = INT_MAX;
     int count = 0;
@@ -14,13 +17,14 @@ int MCM(int pi[], int i, int j)
         if (count < min)
             min = count;
     }
-    return min;
+    return dp[i][j] = min;
 }
 
 int main()
 {
     int arr[] = {1, 2, 3, 4, 3};
     int n = 5;
+    memset(dp, -1, sizeof(dp));
     cout << "Minimum Number of Multiplication is: " << MCM(arr, 1, n - 1);
     return 0;
 }
