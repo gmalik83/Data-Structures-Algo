33cb3644dd88324171b66062b9dd080509b90058
gmalik83 <gaurav98malik@gmail.com>
2021-12-14 21:31:44 +0530

    Sort 0 1 2

---
+++ 450 ques dsa/A9.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Sort an array of 0,1,2
+    /* Count 0 , 1 ,2 and replace it in array
+       use 3 pointer , low->0 mid-> unknown high->end-1
+       case 0 : swap(arr[low++],arr[mid]);
+       case 1: mid++;
+       case 2 : swap(arr[mid],arr[high--]);
+
+    
+    */
+    return 0;
+}
