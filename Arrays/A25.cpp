09f810b99822e1d1d0fdd4fca1b9dd54970e5078
gmalik83 <gaurav98malik@gmail.com>
2022-01-05 23:41:10 +0530

    Changed to O(N)

--- Arrays/A25.cpp
+++ Arrays/A25.cpp
@@ -4,46 +4,35 @@ using namespace std;
 /*
     1. Naive Method : Find First Out of Place element and then find element with opposite sign
     and right rotate subarray .
+    2.Move negative to end of array and then swap with positive O(N) and O(1)
     
 */
-
-void rightrotate(int arr[], int n, int outofplace, int cur)
-{
-    char tmp = arr[cur];
-    for (int i = cur; i > outofplace; i--)
-        arr[i] = arr[i - 1];
-    arr[outofplace] = tmp;
-}
-// Function to rearrange array according to condition
+// Function to Rearrange array
 void rearrange(int arr[], int n)
 {
-    int outofplace = -1;
-    for (int index = 0; index < n; index++)
+    int i = 0, j = n - 1;
+    // Shift negative values to the end
+    while (i < j)
     {
-        if (outofplace >= 0)
-        {
-            if (((arr[index] >= 0) && (arr[outofplace] < 0)) || ((arr[index] < 0) && (arr[outofplace] >= 0)))
-            {
-                rightrotate(arr, n, outofplace, index);
-
-                // the new out-of-place entry is now 2 steps
-                // ahead
-                if (index - outofplace >= 2)
-                    outofplace = outofplace + 2;
-                else
-                    outofplace = -1;
-            }
-        }
-        if (outofplace == -1)
-        {
-            // check if current entry is out-of-place
-            if (((arr[index] >= 0) && (!(index & 0x01))) || ((arr[index] < 0) && (index & 0x01)))
-            {
-                outofplace = index;
-            }
-        }
+        while (i <= n - 1 && arr[i] > 0)
+            i++;
+        while (j >= 0 && arr[i] < 0)
+            j--;
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+    if (i == 0 || i == n)
+        return;
+    // Start with first positive element at index 0
+    int k = 0;
+    while (k < n && i < n)
+    {
+        swap(arr[k], arr[i]);
+        i++;
+        k = k + 2;
     }
 }
+
 // A utility function to print an array 'arr[]' of size 'n'
 void printArray(int arr[], int n)
 {
@@ -53,19 +42,12 @@ void printArray(int arr[], int n)
 }
 int main()
 {
-    int arr[] = {-5, -2, 5, 2,
-                 4, 7, 1, 8, 0, -8};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Given array is \n";
+    int arr[] = {2, 3, -4, -1, 6, -9};
+    int n = 6;
+    cout << "Given array is:";
     printArray(arr, n);
-
     rearrange(arr, n);
-
-    cout << "Rearranged array is \n";
+    cout << "Array after rearranging:";
     printArray(arr, n);
-
-    return 0;
-
     return 0;
 }
