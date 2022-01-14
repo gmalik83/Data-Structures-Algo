99b47ede3d9a975580c95b37db7d68b211013b2c
gmalik83 <gaurav98malik@gmail.com>
2022-01-14 22:38:49 +0530

    Maximum Product Subarray Updated to O(N)

--- Arrays/A28.cpp
+++ Arrays/A28.cpp
@@ -3,30 +3,32 @@ using namespace std;
 // Maximum Product Subarray
 /*
     Solutions: 
-    1. Naive : Consider all subarrays and keep max ans 
+    1. Naive : Consider all subarrays and keep max ans  O(N2) . Expected : O(N) and O(1)
+
 
 */
 // Return the product of maximum product subarray
 int maxSubarrayProduct(int arr[], int n)
 {
-    // Initialize result
-    int res = arr[0];
-    for (int i = 0; i < n; i++)
+    // Max Product ending at Current position
+    int max_ending_here = arr[0];
+    // Min product ending ar current position
+    int min_ending_here = arr[0];
+    // initialize overall max product
+    int max_so_far = arr[0];
+    /*
+        Max product = Max(element , element*max_ending_here , min_ending_here)
+
+    */
+    for (int i = 1; i < n; i++)
     {
-        int mul = arr[i];
-        // Traverse Current Subarray
-        for (int j = i + 1; j < n; j++)
-        {
-            // Update result to keep maximum product
-            res = max(res, mul);
-            mul *= arr[j];
-        }
-        // Update result for n-1 index
-        res = max(res, mul);
+        int temp = max({arr[i], arr[i] * max_ending_here, arr[i] * min_ending_here});
+        min_ending_here = min({arr[i], arr[i] * max_ending_here, arr[i] * min_ending_here});
+        max_ending_here = temp;
+        max_so_far = max(min_ending_here, max_ending_here);
     }
-    return res;
+    return max_so_far;
 }
-// This will be a O(N2) and O(1) complexity as both loop for  every element
 
 int main()
 {
