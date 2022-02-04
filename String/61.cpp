b385c051dd1d1a2a4230d20a8d970b36bdc668ef
gmalik83 <gaurav98malik@gmail.com>
2022-02-05 01:21:25 +0530

    Solution Changed to O(N) complexity

--- String/61.cpp
+++ String/61.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX 256
 
+/*
 // Function to check if string A is shuffled substring of B or not
 
 bool isShuffledSubstring(string A, string B)
@@ -32,6 +34,7 @@ bool isShuffledSubstring(string A, string B)
     }
     return false;
 }
+
 // Driver Code
 int main()
 {
@@ -52,4 +55,51 @@ int main()
         cout << "NO";
     cout << endl;
     return 0;
+}
+*/
+// This function returns true if count of array 1 and array 2 is same
+bool compare(int arr1[], int arr2[])
+{
+    for (int i = 0; i < MAX; i++)
+        if (arr1[i] != arr2[i])
+            return false;
+    return true;
+}
+
+// This search for all permutation of pat in text
+bool search(char *pat, char *txt)
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+    // countP[] : store count of all character of pattern
+    // countTW[] : store count of current window of text
+    int countP[MAX] = {0}, countTW[MAX] = {0};
+    for (int i = 0; i < M; i++)
+    {
+        countP[pat[i]]++;
+        countTW[txt[i]]++;
+    }
+    // Traverse through remaining characters of pattern
+    for (int i = M; i < N; i++)
+    {
+        if (compare(countP, countTW))
+            return true;
+        // Add current character to current window
+        (countTW[txt[i]])++;
+        // Remove first character of previous window
+        countTW[txt[i - M]]--;
+    }
+    if (compare(countP, countTW))
+        return true;
+    return false;
+}
+
+int main()
+{
+    char txt[] = "BACDGABCDA";
+    char pat[] = "ABCD";
+    if (search(pat, txt))
+        cout << "YES";
+    else
+        cout << "NO";
 }
