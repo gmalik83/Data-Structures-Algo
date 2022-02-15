b11453d5ad38b748f64e22c5c83bd7304ee1d402
gmalik83 <gaurav98malik@gmail.com>
2022-02-15 15:47:21 +0530

    Updated to Slide Window

--- String/92.cpp
+++ String/92.cpp
@@ -71,3 +71,65 @@ int main()
     return 0;
 }
 */
+// Function to return string
+string Minimum_Window(string s, string t)
+{
+    int m[256] = {0};
+    // Store variables
+    int ans = INT_MAX;
+    int start = 0;
+    int count = 0;
+    // Create Map
+    for (int i = 0; i < t.length(); i++)
+    {
+        if (m[t[i]] == 0)
+            count++;
+        m[t[i]]++;
+    }
+    // References of Window
+    int i = 0, j = 0;
+    // Traversing the window
+    while (j < s.length())
+    {
+        // Calculations
+        m[s[j]]--;
+        if (m[s[j]] == 0)
+            count--;
+
+        // Condition matching
+        if (count == 0)
+        {
+            while (count == 0)
+            {
+                // Sorting ans
+                if (ans > j - i + 1)
+                {
+                    ans = min(ans, j - i + 1);
+                    start = i;
+                }
+                // Sliding I
+                // Calculation for removing I
+
+                m[s[i]]++;
+                if (m[s[i]] > 0)
+                    count++;
+
+                i++;
+            }
+        }
+        j++;
+    }
+    if (ans != INT_MAX)
+        return s.substr(start, ans);
+    else
+        return "-1";
+}
+int main()
+{
+    string s = "ADOBECODEBANC";
+    string t = "ABC";
+
+    cout << "-->Smallest window that contain all character : " << endl;
+    cout << Minimum_Window(s, t);
+    return 0;
+}
